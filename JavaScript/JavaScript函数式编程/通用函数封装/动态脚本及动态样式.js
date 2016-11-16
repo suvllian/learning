4891f69c34385cee5fc1f139712bb7a89e8fb8d3
Suvllian <suvlliansong@163.com>
2016-11-16 20:30:19 +0800

    Node基础知识及动态加载脚本和样式

---
+++ JavaScript/JavaScript函数式编程/通用函数封装/动态脚本及动态样式.js
@@ -0,0 +1,48 @@
+/*
+动态加载script代码
+*/
+function loadScriptString(code){
+	var script = document.createElement("script");
+	script.type = "text/javascript";
+	try{
+		script.appendChild(document.createTextNode(code));
+	}catch(ex){
+		script.text = code;
+	}
+	document.body.appendChild(script);
+}
+/*
+动态加载css代码
+*/
+function loadStyleString(code){
+	var style = document.createElement("style");
+	style.type = "text/css";
+	try{
+		style.appendChild(document.createTextNode(css));
+	}catch(ex){
+		style.styleSheet.cssText = css;
+	}
+
+	var head = document.getElementsByTagName("head")[0];
+	head.appendChild(style)
+}
+/*
+动态加载外部script
+*/
+function loadScripts(url){
+	var script = document.createElement("script");
+	script.type = "text/javascript";
+	script.src = url;
+	document.body.appendChild(script);
+}
+/*
+动态加载外部css
+*/
+function loadStyles(url){
+	var link = document.createElement("link");
+	link.rel = "stylesheet";
+	link.type = "text/css";
+	link.href = url;
+	var head = document.getElementsByTagName("head")[0];
+	head.appendChild(link);
+}
